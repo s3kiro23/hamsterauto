a277587b8e779c303d454bf5672351cafe5b22ef
Jm GERONIMI <spawn2x@protonmail.com>
2023-02-21 22:01:39 +0100

    [UPD] createsqluser with global intercall

--- app/migrations/create-user.sql
+++ app/migrations/create-user.sql
@@ -4,3 +4,5 @@ CREATE USER 'API_CT'@'%' IDENTIFIED WITH mysql_native_password BY 'Db789!@50';
 GRANT SELECT, DELETE ON `hamsterauto`.* TO 'script_bans'@'%';
 GRANT SELECT, INSERT ON `hamsterauto`.* TO 'API_CT'@'%';
 -- GRANT SELECT, INSERT, UPDATE, DELETE, FILE ON `hamsterauto`.* TO 'db'@'%';
+
+SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
