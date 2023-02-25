ea334243d07b1be8219d4361dc242b960cca0c79
Jm <spawn2x@protonmail.com>
2023-02-25 19:22:29 +0100

    checking bans OK

--- app/script/bans/index.php
+++ app/script/bans/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once ROOT_DIR() . 'src/Entity/Database.php';
+require_once '../../src/Entity/Database.php';
 
 $db = new Database('ban');
 $GLOBALS['Database'] = $db->connexion();
@@ -9,23 +9,24 @@ $currentDate = time();
 
 // requete SQL pour trouver tous les users ayant 3 errors (compte bloqué),
 //  renvoye l'error la plus récente pour pouvoir la comparer au timestamp actuel
-$requete = 'SELECT `login_attempt`.*
-FROM `login_attempt`
-WHERE `login_attempt`.`date` = (SELECT MAX(login_attempt2.date)
-                 	FROM `login_attempt` `login_attempt2`
-                	 WHERE `login_attempt2`.`id_user` = `login_attempt`.`id_user` HAVING COUNT(`login_attempt`.`id_user`)>2)';
-
+$requete = 'SELECT `id_user`, MAX(`generated_at`) AS `last_date`
+    FROM `login_attempt`
+    WHERE `id_user` IN (SELECT `id_user`
+        FROM `login_attempt`
+        GROUP BY `id_user`
+        HAVING COUNT(*) >= 3)
+    GROUP BY `id_user`';
 $result = mysqli_query($GLOBALS['Database'], $requete) or die;
-foreach($result as $count => $user){
-    $dateBan = strtotime($user['date']);
-    //Ci-dessous ajouter a $dateBan une durée au dela de laquelle on veut débloquer le compte (par exemple 15min: +900)
-    $banTimer = $dateBan; 
-    if($banTimer <= $currentDate){
+foreach ($result as $count => $user) {
+    $dateBan = strtotime($user['last_date']);
+    //Ci-dessous ajouter a $dateBan une durée au dela de laquelle on veut débloquer le compte (par exemple 15min: +900 ou 15 * 60)
+    $banTimer = $dateBan + (15 * 60);
+    if ($banTimer <= $currentDate) {
         $requete2 = "DELETE FROM `login_attempt` 
-                    WHERE `login_attempt`.`id_user` = '".mysqli_real_escape_string($GLOBALS['Database'], $user['id_user'])."'";
+            WHERE `login_attempt`.`id_user` = '" . mysqli_real_escape_string($GLOBALS['Database'], $user['id_user']) . "'";
         mysqli_query($GLOBALS['Database'], $requete2) or die;
-        echo 'Compte utilisateur n°'.$user['id_user'].' débloqué <br>';
-    } 
+        error_log('Compte utilisateur n°' . $user['id_user'] . ' débloqué <br>'); 
+    }
 }
 
 //SELECT * FROM `error` WHERE date < "2022-09-27 16:48:00";
