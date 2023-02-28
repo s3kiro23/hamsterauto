2c4ae19c33d77bf62ad27438d7236ce38c42baf8
Jm GERONIMI <spawn2x@protonmail.com>
2023-02-28 19:14:42 +0100

    [FIX] path logo home admin + d-none logo csv on user tab

--- app/templates/admin.php
+++ app/templates/admin.php
@@ -5,11 +5,8 @@ require '../config/Twig.php';
 
 $user = $_SESSION['typeUser'];
 
-if ($user === 'admin' && isset($_SESSION['id'])){
-    echo $twig-> render('admin/admin_office.html.twig');
-}else {
-    header("Location: /error-403");
+if ($user === 'admin' && isset($_SESSION['id'])) {
+    echo $twig->render('admin/admin_office.html.twig');
+} else {
+    header("Location: error-403");
 }
-
-    
-    
