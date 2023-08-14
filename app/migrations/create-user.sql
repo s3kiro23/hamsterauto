5848cc6b4d64dc3208360943225846f7961e164a
Jean-Marie GERONIMI <geronimi@goodbarber.com>
2023-08-14 14:08:52 +0200

    del file .db and sms env

--- app/migrations/create-user.sql
+++
@@ -1,8 +0,0 @@
--- CREATE USER 'db'@'%' IDENTIFIED WITH mysql_native_password BY 'Db123!@20';
-CREATE USER 'ban'@'%' IDENTIFIED WITH mysql_native_password BY 'Db123!@66';
-CREATE USER 'api'@'%' IDENTIFIED WITH mysql_native_password BY 'Db789!@50';
-GRANT SELECT, DELETE ON `hamsterauto`.* TO 'ban'@'%';
-GRANT SELECT, INSERT ON `hamsterauto`.* TO 'api'@'%';
--- GRANT SELECT, INSERT, UPDATE, DELETE, FILE ON `hamsterauto`.* TO 'db'@'%';
-
-SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
